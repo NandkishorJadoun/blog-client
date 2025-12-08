0b307721fd9a7d08943dff0d844af080eb4136e6
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-12-08 10:22:37 +0530

    fix: add url to all loader functions

--- src/assets/js/loaders.js
+++ src/assets/js/loaders.js
@@ -1,15 +1,14 @@
 const API_BASE = "https://api-blogly.onrender.com";
 
 const postList = async () => {
-  const res = await fetch("http://localhost:3000/api/v1/posts");
+  const res = await fetch(`${API_BASE}/api/v1/posts`);
   return res.json();
 };
 
 const post = async ({ params }) => {
   const { postId } = params;
-  const postResponse = await fetch(
-    `${API_BASE}/api/v1/posts/${postId}`
-  );
+
+  const postResponse = await fetch(`${API_BASE}/api/v1/posts/${postId}`);
 
   const post = await postResponse.json();
 
@@ -22,20 +21,17 @@ const post = async ({ params }) => {
 
 const user = async ({ params }) => {
   const { userId } = params;
-  const userResponse = await fetch(
-    `${API_BASE}/api/v1/users/${userId}`
-  );
+  const userResponse = await fetch(`${API_BASE}/api/v1/users/${userId}`);
 
   const user = await userResponse.json();
 
-  const postsPromise = fetch(
-    `${API_BASE}/api/v1/users/${userId}/posts`
-  ).then((r) => r.json());
+  const postsPromise = fetch(`${API_BASE}/api/v1/users/${userId}/posts`).then(
+    (r) => r.json()
+  );
 
   const commentsPromise = fetch(
     `${API_BASE}/api/v1/users/${userId}/comments`
   ).then((r) => r.json());
-
 
   return { user, postsPromise, commentsPromise };
 };
